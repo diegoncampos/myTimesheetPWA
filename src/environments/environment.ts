873d0e78fd4cc8f95f21ede5d99941cd2d7c52df
Diego Campos <diego.nahuel.campos@gmail.com>
2020-10-05 11:50:22 +1300

    Visual changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDnnKFKIY1ygg37kdcjImo-1a5uK-nVKps",
+    authDomain: "mytimesheetpwa.firebaseapp.com",
+    databaseURL: "https://mytimesheetpwa.firebaseio.com",
+    projectId: "mytimesheetpwa",
+    storageBucket: "mytimesheetpwa.appspot.com",
+    messagingSenderId: "1045683988162",
+    appId: "1:1045683988162:web:476498c0c075f276b3bd5b",
+    measurementId: "G-XGBESKWMLJ"
+  }
 };
 
 /*
