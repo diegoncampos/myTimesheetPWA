46edf6b242cbf4811163e04ac05b18502dbac52a
Diego Campos <diego.nahuel.campos@gmail.com>
2020-10-10 21:56:33 +1300

    Fix auto update pwa, prod environment updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDnnKFKIY1ygg37kdcjImo-1a5uK-nVKps",
+    authDomain: "mytimesheetpwa.firebaseapp.com",
+    databaseURL: "https://mytimesheetpwa.firebaseio.com",
+    projectId: "mytimesheetpwa",
+    storageBucket: "mytimesheetpwa.appspot.com",
+    messagingSenderId: "1045683988162",
+    appId: "1:1045683988162:web:476498c0c075f276b3bd5b",
+    measurementId: "G-XGBESKWMLJ"
+  }
 };
